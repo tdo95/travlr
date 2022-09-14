4fdeb199fda70701b0b3fc97dd0077c13e4be449
Tee O <tdopress@gmail.com>
2022-09-14 13:51:00 -0400

    restructured file

--- server.js
+++ server.js
@@ -4,43 +4,39 @@ const app = express();
 const PORT = 3000;
 const MongoClient = require('mongodb').MongoClient;
 
-mongoConnectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vytxyy3.mongodb.net/?retryWrites=true&w=majority`
+let mongoConnectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vytxyy3.mongodb.net/?retryWrites=true&w=majority`;
+let db;
 
 MongoClient.connect(mongoConnectionString)
     .then(client => {
         console.log('Database Connected - YOU DID IT!!!');
-        const db = client.db('travlr-destinations-library');
-        const destinationsCollection = db.collection('destinations');
-
-        //MIDDLEWARE
-        app.use(express.static("public"));
-        app.use(express.json());
-
-        //LANDING PAGE ROUTE
-        app.get('/', (req, res) => {
-            res.send('<h1>WE IN BUSINESS</h1>');
-        })
-        //HOMEPAGE ROUTE
-        app.get('/home', (req, res) => {
-            res.send('<h1>YOU AT THE HOME PAGE NOW BOI</h1>');
-        })
-        //ADD DESTINATION ROUTE
-        app.post('/home', (req, res) => {
-            console.log('ADDING destination....');
-        })
-        //UPDATE DESTINATION ROUTE
-        app.put('/home', (req, res) => {
-            console.log('UPDATING destination....');
-        })
-        //DELETE DESTINATION ROUTE
-        app.put('/home', (req, res) => {
-            console.log('DELETING destination....');
-        })
-
-
-        app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+        db = client.db('travlr-destinations-library');
+        // const destinationsCollection = db.collection('destinations');    
     })
     .catch(err => console.log(err));
 
-
+//MIDDLEWARE
+app.use(express.static("public"));
+app.use(express.json());
+//LANDING PAGE ROUTE
+app.get('/', (req, res) => {
+    res.send('<h1>WE IN BUSINESS</h1>');
+})
+//HOMEPAGE ROUTE
+app.get('/home', (req, res) => {
+    res.send('<h1>YOU AT THE HOME PAGE NOW BOI</h1>');
+})
+//ADD DESTINATION ROUTE
+app.post('/home', (req, res) => {
+    console.log('ADDING destination....');
+})
+//UPDATE DESTINATION ROUTE
+app.put('/home', (req, res) => {
+    console.log('UPDATING destination....');
+})
+//DELETE DESTINATION ROUTE
+app.put('/home', (req, res) => {
+    console.log('DELETING destination....');
+})
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
 
