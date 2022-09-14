cccd0a30a541fdd5c11248223f090d8a51efcf50
Tee O <tdopress@gmail.com>
2022-09-14 11:51:39 -0400

    set up server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('<h1>WE IN BUSINESS</h1>');
+})
+
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
