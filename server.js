119eaaee5d227e33332fd3c5c963987a93a53b5e
Tee O <tdopress@gmail.com>
2022-09-14 12:51:10 -0400

    set up database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const MongoClient = require('mongodb').MongoClient;
+
+mongoConnectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vytxyy3.mongodb.net/?retryWrites=true&w=majority`
+
+// MongoClient.connect(mongoConnectionString)
+//     .then(client => console.log('YOU DID IT!!!'))
+//     .catch(err => console.log(err));
+
+console.log(process.env.MONGO_PASSWORD);
 
 app.use(express.static("public"));
 app.use(express.json());
