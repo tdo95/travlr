b7da938c729edee34c1f2ab1a36737df1c9b0224
Tee O <tdopress@gmail.com>
2022-09-14 13:25:22 -0400

    set up db collection and added route

--- server.js
+++ server.js
@@ -7,16 +7,26 @@ const MongoClient = require('mongodb').MongoClient;
 mongoConnectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vytxyy3.mongodb.net/?retryWrites=true&w=majority`
 
 MongoClient.connect(mongoConnectionString)
-    .then(client => console.log('YOU DID IT!!!'))
+    .then(client => {
+        console.log('Database Connected - YOU DID IT!!!');
+        const db = client.db('travlr-destinations-library');
+        const destinationsCollection = db.collection('destinations');
+
+        //MIDDLEWARE
+        app.use(express.static("public"));
+        app.use(express.json());
+        //LANDING PAGE ROUTE
+        app.get('/', (req, res) => {
+            res.send('<h1>WE IN BUSINESS</h1>');
+        })
+        //HOMEPAGE ROUTE
+        app.get('/home', (req, res) => {
+            res.send('<h1>WE IN BUSINESS</h1>');
+        })
+
+        app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+    })
     .catch(err => console.log(err));
 
 
 
-app.use(express.static("public"));
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('<h1>WE IN BUSINESS</h1>');
-})
-
-app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
