b4212c7c8f203d8ab5ec1b3589c3e87b85607e73
Tee O <tdopress@gmail.com>
2022-09-14 13:32:37 -0400

    added more routes

--- server.js
+++ server.js
@@ -15,14 +15,28 @@ MongoClient.connect(mongoConnectionString)
         //MIDDLEWARE
         app.use(express.static("public"));
         app.use(express.json());
+
         //LANDING PAGE ROUTE
         app.get('/', (req, res) => {
             res.send('<h1>WE IN BUSINESS</h1>');
         })
         //HOMEPAGE ROUTE
         app.get('/home', (req, res) => {
-            res.send('<h1>WE IN BUSINESS</h1>');
+            res.send('<h1>YOU AT THE HOME PAGE NOW BOI</h1>');
         })
+        //ADD DESTINATION ROUTE
+        app.post('/home', (req, res) => {
+            console.log('ADDING destination....');
+        })
+        //UPDATE DESTINATION ROUTE
+        app.put('/home', (req, res) => {
+            console.log('UPDATING destination....');
+        })
+        //DELETE DESTINATION ROUTE
+        app.put('/home', (req, res) => {
+            console.log('DELETING destination....');
+        })
+
 
         app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
     })
