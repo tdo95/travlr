55da75ebf6ae1811d3012ce403261cfbabd0911b
Tee O <tdopress@gmail.com>
2022-09-15 18:34:18 -0400

    implemented post submission with error handling

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 const yearSelect = document.querySelector('#year');
+const newDestinationForm = document.querySelector('#newDestination');
+const newDestinationButton = document.querySelector('#newDestinationButton');
+const newDestinationError = document.querySelector('.newDestinationError')
 
 populateYears();
 
@@ -11,4 +14,58 @@ function populateYears() {
         option.textContent = currentYear + i;
         yearSelect.appendChild(option);
     }
+}
+
+async function validateNewSubmission() {
+    
+    const formItems = Object.values(newDestinationForm);
+    //check that location has been entered
+    if (formItems[1].name === "location" && formItems[1].value === "") {
+        //show error message
+        return showErrorMessage("Please enter a location", newDestinationError);
+    }
+
+    let body = createRequestBody(formItems);
+    
+    await addDestination(body);
+}
+
+function createRequestBody(formItems) {
+    let body = {};
+    for (let field of formItems ) {
+        if (field.name) body[field.name] = field.value;
+    }
+    return body;
+}
+
+async function addDestination(body) {
+    //send the request
+    let response = await fetch('/home', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+    })
+    let data = await response.json();
+
+    //check for error 
+    if (data.error)
+        return showErrorMessage(data.error, newDestinationError);
+    else {
+        //reload page
+        location.reload();
+        //reset the form fields (including clearing error messages)
+        // for (let field of formItems ) {
+        //     if (field.name === "month" || field.name === "year") {
+        //         field.selectedIndex = 0;
+        //     }
+        //     else field.value = "";
+        // }
+        // newDestinationError.innerText = ""
+    }
+
+}
+
+function showErrorMessage(message, item) {
+    //displays error message in the screen provided
+    item.innerText = message;
 }
