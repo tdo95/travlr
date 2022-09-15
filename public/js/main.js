61cfe44244f0eb4dba684020f54f1ec0a42894ea
Tee O <tdopress@gmail.com>
2022-09-15 10:41:02 -0400

    added function to dynamically populate years in select form

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
+const yearSelect = document.querySelector('#year');
 
+populateYears();
+
+//Fill in years on year select form within the add and edit destination forms
+function populateYears() {
+    let currentYear = new Date().getFullYear();
+
+    for (let i = 0; i < 100; i++) {
+        let option = document.createElement('option');
+        option.textContent = currentYear + i;
+        yearSelect.appendChild(option);
+    }
+}
